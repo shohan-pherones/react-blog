b69b60f4e964cb8fbea8f0774f1dce96c9a497ee
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-04 14:28:53 +0600

    error handling

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,34 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPendeing] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
-      .then((data) => setBlogs(data));
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data from the JSON server...");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPendeing(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPendeing(false);
+          setError(err.message);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
     </div>
   );
