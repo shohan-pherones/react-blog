8d1ecbb965fe860f51c313a4437a36a17548e6b3
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-04 14:11:10 +0600

    json server tested

---
+++ src/Bloglist.js
@@ -0,0 +1,17 @@
+const Bloglist = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>
+            Written by <span>{blog.author}</span>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Bloglist;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] =useState([
-    {title: 'JavaScript Coding Championship 2022', body: 'lorem ipsum...', author: 'Coder Boy Kader', id: 1},
-    {title: 'Lipsticks Price Increasing - June Beauty Tips', body: 'lorem ipsum...', author: 'Hot Queen Ayesha', id: 2},
-    {title: 'Girlfriend Experience in Sajek Valley', body: 'lorem ipsum...', author: 'Travel King Motin', id: 3}
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by <span>{blog.author}</span></p>
-        </div>
-      ))}
+      {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
